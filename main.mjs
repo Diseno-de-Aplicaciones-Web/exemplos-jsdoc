3594d209cd1e9fae3996c08945283d7290e985a3
DanBaDo <bytes4life@proton.me>
2023-06-12 09:53:44 +0200

    First commit

---
+++ main.mjs
@@ -0,0 +1,21 @@
+/** @type {String} */
+let texto = "Mira, mira!"
+
+/** @type {(Number|undefined)} */
+let numeroDeVeces
+
+/**
+ * Imprime un texto o número de veces que lle indiquemos
+ * numeroDeVeces ten que ser un número positivo
+ * @param {String} texto 
+ * @param {Number} numeroDeVeces
+ * @returns {Boolean}
+ */
+function imprimirVariasVeces(texto, numeroDeVeces) {
+    if ( numeroDeVeces < 0 ) return false
+    while ( numeroDeVeces ) {
+        console.log(texto)
+        numeroDeVeces = numeroDeVeces-1
+    }
+    return true
+}
